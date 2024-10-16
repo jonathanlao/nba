95bd86505d791ca0cb1fdefbdea9bf9f9a132293
Jonathan Lao <jlao@justworks.com>
2024-10-16 15:35:52 -0400

    Add some tests

--- tests/index.test.ts
+++
@@ -1,7 +0,0 @@
-import '../src/index'; // Adjust the import to your file structure
-
-describe('index.ts', () => {
-  it('should output a message to a file', () => {
-    expect(1).toBe(1);
-  });
-});
